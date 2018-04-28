8761ad30d8e7d1f8fa990cd53400a05c77a8e4d8
hp <hp@10.9.24.147>
2018-04-28 17:10:12 +0300

    Hi group member

--- mart5/src/mart5/Mart5.java
+++ mart5/src/mart5/Mart5.java
@@ -16,7 +16,7 @@ public class Mart5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hi Group member wel come Agile Group Ambo Universityu Woliso Campus");
+        System.out.println("Hi Group member wel come jhhhhhhhhh");
     }
     
 }
